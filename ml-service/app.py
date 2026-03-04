7070614f6ebd3a3a3e541663364d7ecbc36593f6
devendra-code-hub <devendra251india@gmail.com>
2026-03-05 01:47:37 +0530

    change local port for Render

--- ml-service/app.py
+++ ml-service/app.py
@@ -37,5 +37,10 @@ def predict():
         "confidence": round(confidence, 4)
     })
 
+# if __name__ == "__main__":
+#     app.run(port=6000, debug=True)
+import os
+
 if __name__ == "__main__":
-    app.run(port=6000, debug=True)
+    port = int(os.environ.get("PORT", 6000))
+    app.run(host="0.0.0.0", port=port)
